aebf6160c6e25c5e2d6fcc162af7b4aec2a44c71
Cesar Amaro <cesar.amaro@upr.edu>
2018-05-08 17:47:46 -0400

    Changed title screen image

--- src/Main/Launch.java
+++ src/Main/Launch.java
@@ -4,10 +4,9 @@ package Main;
  * Created by Elemental on 12/10/2016.
  */
 public class Launch {
+    
     public static void main(String[] args) {
-        Game game = new Game("2DMagika", 800, 600);
+        Game game = new Game("2D Magicka", 800, 600);
         game.start();
-
     }
-
 }
